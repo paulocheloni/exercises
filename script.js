06a7a9fe6f5f8dc1117f812c07de017f03cfba43
Paulo <paulocheloni@gmail.com>
2020-08-06 15:30:41 -0300

    adicionando filtros em forms com biblioteca Just-validate

--- script.js
+++ script.js
@@ -5,61 +5,70 @@ function createOptions(estados) {
         option.innerHTML = estados[i];
         select.appendChild(option);          
         }
-}
-
-function treatDay(day) {
-    const formatedDay = Number(day.join(''));
-    if (formatedDay < 0 && formatedDay > 31) {
-        alert('formato de dia incorreto');
-    } else {
-        return formatedDay
-    }    
-}
-
-function treatMonth(month) {
-    const formatedMonth = Number(month.join(''));
-    if  (formatedMonth < 0 && formatedMonth > 12) {
-        alert('formato de mes incorreto')
-    } else {
-        return formatedMonth
-    }
-}
-
-function treatYear(year) {
-    const formatedYear = Number(year.join(''));
-    if (formatedYear < 0 && formatedYear > 2100) {
-        alert('formato de ano incorreto')
-    } else {
-        return formatedYear
-    }
-}
-
-function formatDate() {
-    const date = document.getElementById('initial-date').value;
-    let day, month, year = [];
-    for (let i in date) {
-        if(i < 2) {
-            day += date[i];
-        } else if (i > 2 && i < 5) {
-            month += date[i]
-        } else if (i > 5) {
-            year += date[i]
-        }     
-    }
-    return `${treatDay(day)}/${treatMonth(mes)}/${treatYear(year)}`
-}
+}    
 
 function initButtons () {
     const sendForm = document.getElementById('send-form');
     sendForm.addEventListener('submit', function (e) {
         e.preventDefault();    
-        formatDate();
+        
         });
 }
 
 window.onload =  () => {
-    const estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
+    const estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO",
+     "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN",
+      "RO", "RS", "RR", "SC", "SE",
+       "SP", "TO"];
     createOptions(estados);
     initButtons();
+    new JustValidate('.js-form', {
+        rules: {
+            name: {                
+                required: true,
+                minLength: 4,
+                maxLength:15
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            adress: {
+                required:true,    
+            },            
+            cpf: {
+                required:true,
+                remote:
+            },
+            casa: {
+                required:true
+            },
+            city: {
+                required:true
+            },
+        },            
+        messages: {
+                name: {                    
+                    required: 'Nome inválido',
+                    minLength: 'nome com tamanho minimo de 3 caractere',
+                    maxLength: 'nome com tamanho maximo de 15 caractere'
+                },
+                email: 'email inválido'
+        },
+    
+        submitHandler: function (form, values, ajax) {
+        
+            ajax({
+                url: 'https://just-validate-api.herokuapp.com/submit',
+                method: 'POST',
+                data: values,
+                async: true,
+                callback: function(response)  {
+                console.log(response)
+                }
+            });
+        },
+    });
 }
+
 
