aad486eaebc6f05727bac38d6b4de696bd5d68be
Paulo <paulocheloni@gmail.com>
2020-08-18 21:40:30 -0300

    corrigindo lint

--- ex3.js
+++ ex3.js
@@ -1,25 +1,24 @@
 const lesson1 = {
-    materia: 'Matemática',
-    numeroEstudantes: 20,
-    professor: 'Maria Clara',
-    turno: 'manhã',
-  };
-  
-  const lesson2 = {
-    materia: 'História',
-    numeroEstudantes: 20,
-    professor: 'Carlos',
-  };
-  
-  const lesson3 = {
-    materia: 'Matemática',
-    numeroEstudantes: 10,
-    professor: 'Maria Clara',
-    turno: 'noite',
-  };
+  materia: "Matemática",
+  numeroEstudantes: 20,
+  professor: "Maria Clara",
+  turno: "manhã",
+};
 
-  const insertProperty = (obj, property, value) => {      
-      obj.property = value
-  }
-  insertProperty(lesson2, 'turno', 'manhã');
-  
+const lesson2 = {
+  materia: "História",
+  numeroEstudantes: 20,
+  professor: "Carlos",
+};
+
+const lesson3 = {
+  materia: "Matemática",
+  numeroEstudantes: 10,
+  professor: "Maria Clara",
+  turno: "noite",
+};
+
+const insertProperty = (obj, property, value) => {
+  obj.property = value;
+};
+insertProperty(lesson2, "turno", "manhã");
