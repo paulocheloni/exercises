37ebb5a78de0b678bd1ee7754681c13a92df89cf
Paulo <paulocheloni@gmail.com>
2020-08-19 01:39:10 -0300

    corrigindo lint em ex8

--- ex8.js
+++ ex8.js
@@ -1,29 +1,31 @@
 const lesson1 = {
-    materia: "Matemática",
-    numeroEstudantes: 20,
-    professor: "Maria Clara",
-    turno: "manhã",
-  };
-  
-  const lesson2 = {
-    materia: "História",
-    numeroEstudantes: 20,
-    professor: "Carlos",
-  };
-  
-  const lesson3 = {
-    materia: "Matemática",
-    numeroEstudantes: 10,
-    professor: "Maria Clara",
-    turno: "noite",
-  };  
-  
-  
-  const allLessons = Object.assign({lesson1: lesson1, lesson2: lesson2, lesson3: lesson3});
-  const mathClass = Object.keys(allLessons)
-  .filter(element => allLessons[element].materia === 'Matemática')
-  .reduce((acc, element) => allLessons[acc].numeroEstudantes + allLessons[element].numeroEstudantes);
-  
- 
-  
+  materia: "Matemática",
+  numeroEstudantes: 20,
+  professor: "Maria Clara",
+  turno: "manhã",
+};
 
+const lesson2 = {
+  materia: "História",
+  numeroEstudantes: 20,
+  professor: "Carlos",
+};
+
+const lesson3 = {
+  materia: "Matemática",
+  numeroEstudantes: 10,
+  professor: "Maria Clara",
+  turno: "noite",
+};
+
+const allLessons = Object.assign({
+  lesson1: lesson1,
+  lesson2: lesson2,
+  lesson3: lesson3,
+});
+const mathClass = Object.keys(allLessons)
+  .filter((element) => allLessons[element].materia === "Matemática")
+  .reduce(
+    (acc, element) =>
+      allLessons[acc].numeroEstudantes + allLessons[element].numeroEstudantes
+  );
