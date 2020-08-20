9b86fc245b172d5eb8e6194ee88f28ac954790b4
Paulo <paulocheloni@gmail.com>
2020-08-20 14:51:14 -0300

    adicionando exercicio 1 parte 2 escrevendo funçoes para teste

---
+++ TDD/src/addOne.js
@@ -0,0 +1,6 @@
+const addOne = (array) => {
+  const addedOne = array.map((element) => element + 1);
+  return addedOne
+};
+
+module.exports = addOne;
--- myFizzBuzz.js
+++ myFizzBuzz.js
@@ -1,20 +1,20 @@
-const assert = require('assert');
-const { isMainThread } = require('worker_threads');
+const assert = require("assert");
+const { isMainThread } = require("worker_threads");
 
 function myFizzBuzz(num) {
-  if (typeof num !== 'number') return false;
-  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
-  if (num % 3 === 0) return 'fizz';
-  if (num % 5 === 0) return 'buzz';
+  if (typeof num !== "number") return false;
+  if (num % 3 === 0 && num % 5 === 0) return "fizzbuzz";
+  if (num % 3 === 0) return "fizz";
+  if (num % 5 === 0) return "buzz";
   return num;
 }
 
-const fizzbuzz = myFizzBuzz(15)
-assert.ok(fizzbuzz === 'fizzbuzz');
+const fizzbuzz = myFizzBuzz(15);
+assert.ok(fizzbuzz === "fizzbuzz");
 const fizz = myFizzBuzz(3);
-assert.ok(fizz === 'fizz' )
+assert.ok(fizz === "fizz");
 const buzz = myFizzBuzz(10);
-assert.ok(buzz === 'buzz');
-const notFizz = (myFizzBuzz(8));
-assert.ok(notFizz === 8)
+assert.ok(buzz === "buzz");
+const notFizz = myFizzBuzz(8);
+assert.ok(notFizz === 8);
 // implemente seus testes aqui
