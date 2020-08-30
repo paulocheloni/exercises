8d1a3e19125b1cfbb9e13371008de73d4d40ad2c
Paulo <paulocheloni@gmail.com>
2020-08-30 12:13:51 -0300

    adicionando conta autores de mesma idade

---
+++ SameBirthYear.js
@@ -0,0 +1,87 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expected_result = false;
+
+function authorUnique() {
+  
+    
+  const sameAge = books.map(function (book) {
+    const birthYear = book.author.birthYear;
+    let countSame = 0
+      books.forEach(eachBook => { 
+        if(eachBook.author.birthYear === birthYear) {
+            countSame += 1
+        } 
+    });
+    return countSame; 
+  });
+  if (sameAge >= 2) {
+      return true;
+  }
+  return false
+}
+
+assert.equal(authorUnique(), expected_result);
