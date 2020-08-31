d99eb9fa460f0152fe27325331ef2ef2abb1b9a9
Paulo <paulocheloni@gmail.com>
2020-08-31 10:00:37 -0300

    adicionando filterPeople - filter with objectDestructuring

---
+++ filterPeople.js
@@ -0,0 +1,38 @@
+const assert = require('assert')
+
+const people = [
+    {
+        name: "Nicole",
+        bornIn: 1992,
+        nationality: "Australian"
+    },
+    {
+        name: "Harry",
+        bornIn: 2008,
+        nationality: "Australian"
+    },
+    {
+        name: "Toby",
+        bornIn: 1901,
+        nationality: "Australian"
+    },
+    {
+        name: "Frida",
+        bornIn: 1960,
+        nationality: "Dannish"
+    },
+    {
+        name: "Fernando",
+        bornIn: 2001,
+        nationality: "Brazilian"
+    }
+]
+const isBornInXX = (bornIn) => bornIn <= 2000 && bornIn >= 1901;
+const isAustralian = (nationality) => nationality === 'Australian';
+const filterPeople = (people) => people.filter( 
+    ({bornIn, nationality}) => isBornInXX(bornIn) && isAustralian(nationality) );
+
+const filteredPeople = filterPeople(people)
+
+assert.deepEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
