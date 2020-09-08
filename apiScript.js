92b7f50521f980139ab72acf0ec7d68e847e533e
Paulo <paulocheloni@gmail.com>
2020-09-07 22:41:02 -0300

    refactoring exercise of fetch api

--- apiScript.js
+++ apiScript.js
@@ -1,28 +1,13 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = (apiURL, object) => {
-  return new Promise((resolve, reject) => {
-    if (apiURL === 'https://icanhazdadjoke.com/') {
-      fetch(apiURL, object)
-        .then((response) => response.json())
-        .then((data) => resolve(data));
-    } else {
-      reject(new Error('No ending point'));
-    }
-  });
+const fetchJoke = async (apiURL, object) => {
+  const responseObject = (await fetch (apiURL, object)).json()
+  document.querySelector('#jokeContainer').innerText = responseObject.joke  
 };
 const object = {
     method: 'GET',
     headers: { 'Accept': 'application/json' }
 };
 
-const showJoke = async () => {
-  const { joke } = await fetchJoke(API_URL, object);
-  const jokeContainer = document.createElement('span');
-  const body = document.querySelector('body');
-  jokeContainer.innerText = joke;
-  jokeContainer.classList = 'joke';
-  body.appendChild(jokeContainer);
-};
 
-window.onload = () => showJoke();
+window.onload = () => fetchJoke(API_URL, object);
---
+++ getAnimals.spec.js
@@ -0,0 +1,9 @@
+const getListAnimals = require('./getAnimals');
+
+describe('Quando o tipo do animal, existe', () => {
+    test('Retorne a lista de animais', () => {
+        getListAnimals('Dog').then(listDogs => {
+            expect('')
+        });
+    });
+  });
