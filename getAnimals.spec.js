af50b8da8d93cbf9bc556fbf5691e82bcb6fc367
Paulo <paulocheloni@gmail.com>
2020-09-07 21:16:21 -0300

    adding get animals

---
+++ getAnimals.spec.js
@@ -0,0 +1,11 @@
+const getListAnimals = require('./getAnimals');
+
+describe('Quando o tipo do animal existe', () => {
+    test('Retorne a lista de animais', () => {
+      expect.assertions(2);
+      return getListAnimals('Dog').then(listDogs => {
+        expect(listDogs[0].name).toEqual('Dorminhoco');
+        expect(listDogs[1].name).toEqual('Soneca');
+      });
+    });
+  });
