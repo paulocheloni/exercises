34f2b6ddbc2a0c3f83ca1e20b6521ada80eb98ab
Paulo <paulocheloni@gmail.com>
2020-09-09 12:06:08 -0300

    update commit

--- apiScript.js
+++ apiScript.js
@@ -1,13 +1,11 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = async (apiURL, object) => {
-  const responseObject = (await fetch (apiURL, object)).json()
+  const responseObject = await (await fetch (apiURL, object)).json()  
   document.querySelector('#jokeContainer').innerText = responseObject.joke  
 };
 const object = {
     method: 'GET',
     headers: { 'Accept': 'application/json' }
 };
-
-
 window.onload = () => fetchJoke(API_URL, object);
--- getAnimals.spec.js
+++ getAnimals.spec.js
@@ -1,9 +1,10 @@
 const getListAnimals = require('./getAnimals');
 
 describe('Quando o tipo do animal, existe', () => {
-    test('Retorne a lista de animais', () => {
-        getListAnimals('Dog').then(listDogs => {
-            expect('')
+    test('Retorne a lista de animais', async () => {
+        const listDogs = await getListAnimals('Dog');
+            expect(listDogs[0].name ).toEqual('Dorminhoco');
+            expect(listDogs[1].name).toEqual('Soneca');
         });
     });
-  });
+  
